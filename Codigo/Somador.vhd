e3a574c8c4e5bd60a034432162ef184b1ac683dc
João Pedro Winckler Bernardi <jpwbernardi@hotmail.com>
2015-05-15 01:15:58 -0300

    Organizando 3

--- Codigo/Somador.vhd
+++ Codigo/Somador.vhd
@@ -3,35 +3,17 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.numeric_std_unsigned.all;
 
-entity Soma is
+entity Somador is
   Port(
     num1: in std_logic_vector(31 downto 0);
     num2: in std_logic_vector(31 downto 0);
     resp1: out std_logic_vector(31 downto 0)
   );
-end Soma;
+end Somador;
 
-architecture comp of Soma is
-begin {library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_arith.all;
-use IEEE.numeric_std_unsigned.all;
-
-entity Soma is
-  Port(
-    num1: in std_logic_vector(31 downto 0);
-    num2: in std_logic_vector(31 downto 0);
-    resp1: out std_logic_vector(31 downto 0)
-  );
-end Soma;
-
-architecture comp of Soma is
+architecture comp of Somador is
 begin 
   resp1 <= num1 + num2;
 end comp;
 
-
-}
-  resp1 <= num1 + num2;
-end comp;
 
