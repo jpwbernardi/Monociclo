86952016a4a14a1da6644c735fd6134d3dc5c8dd
João Pedro Winckler Bernardi <jpwbernardi@hotmail.com>
2015-05-16 01:01:16 -0300

    Esqueleto do controle adicionado

--- Codigo/Controle.vhd
+++ Codigo/Controle.vhd
@@ -4,10 +4,156 @@ use IEEE.numeric_std.all;
 
 entity Controle is
   Port(
-    bla: in std_logic
+    Instruction: in signed(5 downto 0);
+    ALUOp1, ALUOp2, Branch, MemRead, MemtoReg, jump, MemWrite, ALUSrc, RegWrite, BEQ, jal, jr, RegDst : out std_logic
   );
 end Controle;
 
 architecture cmp of Controle is
 begin
+  process(Instruction)
+  begin
+    case Instruction is
+      when "000000" =>  --Tipo R
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "000101" =>  --BNE
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "000100" =>  --BEQ
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "001100" => --SLTI
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "100011" => --Load
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "101011" => --SW
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "001000" => --ADDI
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "000010" => --Jump
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when "000011" =>
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+      when others =>
+        RegDst <= '0';
+        Branch <= '0';
+        jump <= '0';
+        MemRead <= '0';
+        MemtoReg <= '0';
+        ALUOp1 <= '0';
+        ALUOp2 <=  '0';
+        MemWrite <= '0';
+        ALUSrc  <= '0';
+        RegWrite <= '0';
+        jr <= '0';
+        jal <= '0';
+        BEQ <= '0';
+    end case;
+  end process;
 end cmp;
