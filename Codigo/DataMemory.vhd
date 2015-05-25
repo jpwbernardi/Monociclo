5ce99911bc9eac3cb9905c1ee69f11eeeb15acfc
João Pedro Winckler Bernardi <jpwbernardi@hotmail.com>
2015-05-24 22:00:37 -0300

    Raaararaaa

--- Codigo/DataMemory.vhd
+++ Codigo/DataMemory.vhd
@@ -7,7 +7,7 @@ entity DataMemory is --Instruction Memory
   Port(
     Adress: in signed(31 downto 0);
     WriteD: in signed(31 downto 0);
-    MemWrite1, MemRead1: in std_logic;
+    MemWrite1, MemRead1, cloque: in std_logic;
     ReadData1: out signed(31 downto 0)
   );
 end DataMemory;
@@ -15,17 +15,17 @@ end DataMemory;
 architecture cmp of DataMemory is
 signal memoria: VetorWord(0 to 63);  --256 Bytes de memória  (64 * 32 bits)
 begin
-  process(WriteD, Adress, MemWrite1)
+  process(cloque, MemWrite1)
   begin
-    if(MemWrite1 = '1') then
-      memoria(to_integer(Adress)) <= WriteD;
-    end if;
+      if((cloque'event and cloque = '0') and MemWrite1 = '1') then
+        memoria(to_integer(Adress(31 downto 3))) <= WriteD;
+      end if;
   end process;
   
   process(MemRead1, Adress)
   begin
     if(MemRead1 = '1') then
-      ReadData1 <= memoria(to_integer(Adress)); 
+      ReadData1 <= memoria(to_integer(Adress(31 downto 3))); 
     end if;
   end process;
 end cmp;
